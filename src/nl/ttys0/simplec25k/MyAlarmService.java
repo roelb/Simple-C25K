ed515dd6c37fdbd3898482eb9b98cb8c5844f0ba
Roel Blaauwgeers <roel@ttys0.nl>
2012-03-20 18:44:25 +0100

    updated

--- src/nl/ttys0/simplec25k/MyAlarmService.java
+++ src/nl/ttys0/simplec25k/MyAlarmService.java
@@ -1,27 +1,24 @@
 /*
- * MyAlarmService.java
+ *  MyAlarmService.java
  * 
- * Copyright 2012 Roel Blaauwgeers <rs.blaauwgeers@gmail.com>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
- * 
+ *  Copyright 2012 Roel Blaauwgeers <roel@ttys0.nl>
  *
- * info:
- * This Class is used for setting an alarm. When the alarm goes of a defined message
- * (MESSAGE) will be send to the notification bar.
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ *  info:
+ *  This Class is used for setting an alarm. When the alarm goes of a defined message
+ *  (MESSAGE) will be send to the notification bar.
  */
 
 package nl.ttys0.simplec25k;
